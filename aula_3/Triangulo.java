becc6587154e6da58ad908838f0f20651c2441e3
Rafael <rafaeldavi8086@gmail.com>
2024-10-23 19:12:23 -0300

    Implementação exercícios aula 3

---
+++ aula_3/Triangulo.java
@@ -0,0 +1,25 @@
+package aula_3;
+
+public class Triangulo {
+  private double valor1;
+  private double valor2;
+  private double valor3;
+
+  public Triangulo(double valor1, double valor2, double valor3) {
+    this.valor1 = valor1;
+    this.valor2 = valor2;
+    this.valor3 = valor3;
+  }
+
+  public double getValor1() {
+    return valor1;
+  }
+
+  public double getValor2() {
+    return valor2;
+  }
+
+  public double getValor3() {
+    return valor3;
+  }
+}
